ecbc7bd89e59d6bc60356641ad309e92bed1eb67
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-11 22:42:57 +0200

    add data subfolders

--- bash/init.sh
+++ bash/init.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # code to set up the repo
 
-cd ../
-
 mkdir data figures scripts R
 
 for folder in data figures scripts
 do
 	echo "files related to $folder" > $folder/_description.txt
+    if [ $folder = "data" ]
+    then
+        cd $folder
+        mkdir raw spatial results
+        for subdir in raw spatial results
+        do 
+            echo "data related to $subdir" > $subdir/_description.txt
+        done
+        cd ..
+    fi
 done
